6eb58aa18ecb477e960205220e663d78e095c335
Stella <lottie798ko@gmail.com>
2026-08-18 03:46:26 +0630

    Fix duplicate entry in MENU links

--- txt/menu.js
+++ txt/menu.js
@@ -2,7 +2,9 @@ window.MENU = [
     {
         category: "Guide",
         links: [
-            { fileId: "01", name: "Transfeminine များအတွက် HRT Sublingual/Buccal လမ်းညွှန်အကျဉ်း" }
+            { fileId: "01", name: "Transfeminine များအတွက် HRT Sublingual/Buccal လမ်းညွှန်အကျဉ်း" }, 
+            { fileId: "02", name: "ကုန်ကျမည့်ငွေပမာဏများ" }
+            
             // Add more: { fileId: "02", name: "Anti-Androgens" }
         ]
     }
