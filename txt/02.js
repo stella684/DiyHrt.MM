a183cc564e6ddb67a8e2d5c26fe73f6c29fde1af
Stella <lottie798ko@gmail.com>
2026-08-18 03:50:10 +0630

    Update text formatting and content in 02.js

--- txt/02.js
+++ txt/02.js
@@ -1,6 +1,6 @@
 window.PAGES = window.PAGES || {};
 window.PAGES['02'] = `
-# ကုန်ကျမည့်ငွေပမာဏများ
+[color:#e3b341]# ကုန်ကျမည့်ငွေပမာဏများ[/color]
 
 ## Estrogen ဆေးများ
 • estrofem 2mg 28 လုံး - 40000 MMK  
@@ -10,7 +10,7 @@ window.PAGES['02'] = `
 ## Anti-androgen ဆေးများ
 • androcur 40mg 10 လုံး - 77000 MMK  
 • gale 40mg 10 လုံး - 60000 MMK  
-- လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက်40စာရမှာဖြစ်ပြီး နှစ်ရက်တစ်ပိုင်းဆို ရက်80စာရပါမယ်
+• လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက်40စာရမှာဖြစ်ပြီး နှစ်ရက်တစ်ပိုင်းဆို ရက်80စာရပါမယ်
 
 ## အသေးစိတ်တွင်ထားတာကို :
 [color:#58a6ff]https://docs.google.com/spreadsheets/d/15OTRzg8fjEEjZkzfu91qEgNIPWOTp5JYYqhSIk86t5Q/edit?gid=0#gid=0[/color]  
