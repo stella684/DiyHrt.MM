fd51c693d574f413bd2be207ad5ca05a8b838cb2
Stella <lottie798ko@gmail.com>
2026-08-18 03:59:55 +0630

    Update 02.js

--- txt/02.js
+++ txt/02.js
@@ -1,22 +1,22 @@
 window.PAGES = window.PAGES || {};
 window.PAGES['02'] = `
-ကုန်ကျမည့်ငွေပမာဏများ
+## ကုန်ကျမည့်ငွေပမာဏများ
 
-Estrogen ဆေးများ
-- estrofem 2mg 28 လုံး - 40000 MMK
-- progynova 2mg 28 လုံး - 35000 MMK
-- progynova 2mg 3ကဒ် ( 28 x 3 ) - 97000 MMK
+### Estrogen ဆေးများ
+- **estrofem 2mg 28 လုံး** - 40000 MMK
+- **progynova 2mg 28 လုံး** - 35000 MMK
+- **progynova 2mg 3ကဒ် ( 28 x 3 )** - 97000 MMK
 
-Anti-androgen ဆေးများ
-- androcur 40mg 10 လုံး - 77000 MMK
-- gale 40mg 10 လုံး - 60000 MMK
-- လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက်40စာရမှာဖြစ်ပြီး နှစ်ရက်တစ်ပိုင်းဆို ရက်80စာရပါမယ်
+### Anti-androgen ဆေးများ
+- **androcur 40mg 10 လုံး** - 77000 MMK
+- **gale 40mg 10 လုံး** - 60000 MMK
+- *လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက် 40 စာရမှာဖြစ်ပြီး၊ နှစ်ရက်တစ်ပိုင်းဆို ရက် 80 စာရပါမယ်။*
 
-အသေးစိတ်တွက်ထားတာကို :
+---
+
+အသေးစိတ်ကို အောက်ပါ link တွင် ကြည့်ရှုနိုင်ပါသည်:
 https://docs.google.com/spreadsheets/d/15OTRzg8fjEEjZkzfu91qEgNIPWOTp5JYYqhSIk86t5Q/edit?gid=0#gid=0
-ထဲမှာကြည့်လို့ရပါတယ်
 
-Estrofem နဲ့ Anti-androgen ကိုဈေးအသက်သာဆုံး
+Estrofem နဲ့ Anti-androgen ကို ဈေးအသက်သာဆုံး ဝယ်ယူနိုင်ရန်:
 https://t.me/sumflower245
-မှာဝယ်ယူနိုင်ပါတယ်
 `;
