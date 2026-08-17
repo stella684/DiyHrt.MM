f69840051090eb0a929eaa82449bd3f2b94e7947
Stella <lottie798ko@gmail.com>
2026-08-18 03:29:06 +0630

    Add comment for future links in menu.js
    Added a comment for future link addition in the menu.

--- txt/menu.js
+++ txt/menu.js
@@ -3,6 +3,7 @@ window.MENU = [
         category: "Guide",
         links: [
             { fileId: "01", name: "Transfeminine များအတွက် HRT Sublingual/Buccal လမ်းညွှန်အကျဉ်း" }
+            // Add more: { fileId: "02", name: "Anti-Androgens" }
         ]
     }
 ];
