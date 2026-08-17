2f9296a9790f2795c2693f4b7dbc51b1395267d5
Stella <lottie798ko@gmail.com>
2026-08-18 03:54:38 +0630

    Update 02.js

--- txt/02.js
+++ txt/02.js
@@ -1,22 +1,5 @@
-window.PAGES = window.PAGES || {};
-window.PAGES['02'] = `
-# ကုန်ကျမည့်ငွေပမာဏများ
-
-## Estrogen ဆေးများ
-• estrofem 2mg 28 လုံး - 40000 MMK  
-• progynova 2mg 28 လုံး - 35000 MMK  
-• progynova 2mg 3ကဒ် ( 28 x 3 )  - 97000 MMK  
-
-## Anti-androgen ဆေးများ
-• androcur 40mg 10 လုံး - 77000 MMK  
-• gale 40mg 10 လုံး - 60000 MMK  
-• လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက်40စာရမှာဖြစ်ပြီး နှစ်ရက်တစ်ပိုင်းဆို ရက်80စာရပါမယ်
-
-## အသေးစိတ်တွင်ထားတာကို :
-[color:#58a6ff]https://docs.google.com/spreadsheets/d/15OTRzg8fjEEjZkzfu91qEgNIPWOTp5JYYqhSIk86t5Q/edit?gid=0#gid=0[/color]  
-ထဲမှာကြည့်လို့ရပါတယ်
-
-## Estrofem နဲ့ Anti-androgen ကိုဈေးအသက်သာဆုံး
-[color:#58a6ff]https://t.me/sumflower245[/color]  
-မှာဝယ်ယူနိုင်ပါတယ်
-`;
+{
+  "fileId": "02",
+  "name": "ကုန်ကျမည့်ငွေပမာဏများ",
+  "content": "Estrogen ဆေးများ\n- estrofem 2mg 28 လုံး - 40000 MMK\n- progynova 2mg 28 လုံး - 35000 MMK\n- progynova 2mg 3ကဒ် ( 28 x 3 ) - 97000 MMK\n\nAnti-androgen ဆေးများ\n- androcur 40mg 10 လုံး - 77000 MMK\n- gale 40mg 10 လုံး - 60000 MMK\n- လေးပိုင်းပိုင်းလိုက်ပြီး တစ်ရက်တစ်ပိုင်းဆို ရက်40စာရမှာဖြစ်ပြီး နှစ်ရက်တစ်ပိုင်းဆို ရက်80စာရပါမယ်\n\nအသေးစိတ်တွင်ထားတာကို :\nhttps://docs.google.com/spreadsheets/d/15OTRzg8fjEEjZkzfu91qEgNIPWOTp5JYYqhSIk86t5Q/edit?gid=0#gid=0\nထဲမှာကြည့်လို့ရပါတယ်\n\nEstrofem နဲ့ Anti-androgen ကိုဈေးအသက်သာဆုံး\nhttps://t.me/sumflower245\nမှာဝယ်ယူနိုင်ပါတယ်"
+}
