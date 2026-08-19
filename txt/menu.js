a0cc96b2cbe9cb36312b9dc4ac74073602f3a731
Stella <lottie798ko@gmail.com>
2026-08-19 17:21:01 +0630

    Update menu.js

--- txt/menu.js
+++ txt/menu.js
@@ -2,15 +2,11 @@ window.MENU = [
     {
         category: "Guide Transfeminine",
         links: [
+            { fileId: "05", name: "Disclaimer & မိတ်ဆက် }
             { fileId: "01", name: "Transfeminine များအတွက် HRT Sublingual/Buccal လမ်းညွှန်အကျဉ်း" }, 
             { fileId: "02", name: "ကုန်ကျမည့်ငွေပမာဏများ" }, 
             { fileId: "03", name: "ခန္ဓာကိုယ်ပြောင်းလဲမှုများ" }, 
             { fileId: "04", name: "သွေးစစ်ခြင်းလမ်းညွှန်" }
-            // Add more: { fileId: "02", name: "Anti-Androgens" }
-        ], 
-        category: "Extra",
-        links: [
-            { fileId: "05", name: "Disclaimer & Others" }
             // Add more: { fileId: "02", name: "Anti-Androgens" }
         ]
     }
