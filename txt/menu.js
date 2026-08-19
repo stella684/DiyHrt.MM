e802274ffe99301665c284bbba7b1782950a30cf
Stella <lottie798ko@gmail.com>
2026-08-19 17:01:04 +0630

    Update menu.js with new links and formatting
    Added a new link for blood test guidance and corrected spacing.

--- txt/menu.js
+++ txt/menu.js
@@ -4,7 +4,8 @@ window.MENU = [
         links: [
             { fileId: "01", name: "Transfeminine များအတွက် HRT Sublingual/Buccal လမ်းညွှန်အကျဉ်း" }, 
             { fileId: "02", name: "ကုန်ကျမည့်ငွေပမာဏများ" }, 
-            { fileId: "03", name: " ခန္ဓာကိုယ်ပြောင်းလဲမှုများ" }
+            { fileId: "03", name: "ခန္ဓာကိုယ်ပြောင်းလဲမှုများ" }, 
+            { fileId: "04", name: "သွေးစစ်ခြင်းလမ်းညွှန်" }
             // Add more: { fileId: "02", name: "Anti-Androgens" }
         ]
     }
