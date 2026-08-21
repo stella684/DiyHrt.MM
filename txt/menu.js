47d03cbaa69ee3cc6470b30c08b08ec9cebb8607
Stella <lottie798ko@gmail.com>
2026-08-21 23:13:46 +0630

    Update menu.js

--- txt/menu.js
+++ txt/menu.js
@@ -12,10 +12,16 @@ window.MENU = [
   {
     category: "Transfeminine ( Sublingual/Buccal )",
     links: [
-      { fileId: "05", name: "မိတ်ဆက်" },
+      { fileId: "05", name: "Sublingual/Buccal မိတ်ဆက်" },
       { fileId: "01", name: "Sublingual/Buccal အပြည့်အစုံ" },
       { fileId: "02", name: "ကုန်ကျငွေ" }, 
       { fileId: "04", name: "သွေးစစ်ခြင်း" }
     ]
-  }
+  }, 
+  {
+    category: "Transfeminine ( Injections ) ",
+    links: [
+      { fileId: "injection-introduction" , name: "Coming Soon" }
+    ]
+  },
 ];
